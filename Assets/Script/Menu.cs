413253702f696b27de4311ffe586f175c1c5c4b8
Jyotinder <jjyotind@asu.edu>
2016-10-29 11:13:20 -0700

    mainMenu

---
+++ Assets/Script/Menu.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+    private Animator _animation;
+    private CanvasGroup _canvas;
+
+    public bool IsOpen {
+        get { return _animation.GetBool("IsOpen"); }
+        set { _animation.SetBool("IsOpen",value); }
+    }
+
+
+    public void Awake() {
+        _animation = GetComponent<Animator>();
+        _canvas = GetComponent<CanvasGroup>();
+        var rect = GetComponent<RectTransform>();
+        rect.offsetMax = rect.offsetMin = new Vector2(0, 0);
+
+    }
+
+
+
+    public void Update() {
+        if (!_animation.GetCurrentAnimatorStateInfo(0).IsName("Open")) {
+            _canvas.blocksRaycasts = _canvas.interactable = false;
+        }else
+            _canvas.blocksRaycasts = _canvas.interactable = true;
+    }
+}
---
+++ Assets/Script/MenuManger.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuManger : MonoBehaviour {
+
+    public Menu CurrentMenu;
+    public void Start() {
+        ShowMenu(CurrentMenu);
+    }
+
+    public void ShowMenu(Menu menu) {
+        if (CurrentMenu != null) {
+            CurrentMenu.IsOpen = false;
+            CurrentMenu = menu;
+            CurrentMenu.IsOpen = true;
+        }
+    }
+
+    public void quit()
+    {
+       
+        Application.Quit();
+
+    }
+}
