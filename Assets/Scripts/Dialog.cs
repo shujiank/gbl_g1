684743ff1930b0dfc9a9dcf42d53cedb93ea0203
Jyotinder <jjyotind@asu.edu>
2016-11-06 14:26:30 -0800

    added collionBox on NPC and created OnTriggerEnter function

---
+++ Assets/Scripts/Dialog.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dialog : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other) {
+        Debug.Log("collision happened");
+    }
+}
